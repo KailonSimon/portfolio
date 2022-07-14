01e4a09878ce433147039e0eeb5d6a0e5bd858a2
KailonSimon <81045491+KailonSimon@users.noreply.github.com>
2022-07-14 05:38:55 -0500

    Add navbar animation for mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 import NavLinks from "./NavLinks";
 import Socials from "./Socials";
 
-function Navbar() {
-  const [opened, setOpened] = useState(false);
+export default function Navbar() {
+  const [opened, setOpened] = useState(true);
 
   return (
     <header>
@@ -15,7 +15,7 @@ function Navbar() {
           </a>
         </div>
         <div className="nav-menu" id="nav-menu">
-          <NavLinks handleClick={() => setOpened(false)} />
+          <NavLinks mobile={false} />
         </div>
         <div className="nav-socials">
           <Socials />
@@ -31,16 +31,14 @@ function Navbar() {
         opened={opened}
         onClose={() => setOpened(false)}
         size="full"
-        position="right"
         withOverlay={false}
+        transition="slide-down"
       >
         <div className="drawer-content">
-          <NavLinks />
+          <NavLinks mobile={true} handleClick={() => setOpened(!opened)} />
           <Socials />
         </div>
       </Drawer>
     </header>
   );
 }
-
-export default Navbar;
