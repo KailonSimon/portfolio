40cdc78575b34317fae332fa4f4a13f55d6835dc
KailonSimon <81045491+KailonSimon@users.noreply.github.com>
2022-07-14 05:40:30 -0500

    Add animation to nav

--- components/NavLinks.js
+++ components/NavLinks.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-scroll";
+import { motion, AnimatePresence, useAnimation } from "framer-motion";
 
 const links = [
   { href: "hero", text: "Home" },
   { href: "about", text: "About" },
+  { href: "projects", text: "Projects" },
 ];
 
-function NavLinks({ handleClick }) {
+const list = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+      delayChildren: 0.25,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, x: 50 },
+  show: { opacity: 1, x: 0 },
+};
+
+function NavLinks({ mobile, handleClick }) {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start();
+  }, [controls]);
+
+  if (mobile) {
+    return (
+      <motion.ul
+        className="nav-list"
+        variants={list}
+        initial="hidden"
+        animate="show"
+      >
+        {links.map((link) => {
+          return (
+            <motion.li className="nav-item" key={link.text} variants={item}>
+              <Link
+                to={link.href}
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={250}
+                activeClass="active"
+                onClick={handleClick}
+              >
+                {link.text}
+              </Link>
+            </motion.li>
+          );
+        })}
+        <motion.li className="nav-item" variants={item}>
+          <a href={"/resume.pdf"} target="_blank" rel="noreferrer">
+            Resume
+          </a>
+        </motion.li>
+      </motion.ul>
+    );
+  }
   return (
     <ul className="nav-list">
       {links.map((link) => {
@@ -16,10 +73,10 @@ function NavLinks({ handleClick }) {
               to={link.href}
               spy={true}
               smooth={true}
-              offset={-75}
-              duration={100}
+              offset={-100}
+              duration={250}
               activeClass="active"
-              onSetActive={handleClick}
+              //onSetActive={handleClick}
             >
               {link.text}
             </Link>
@@ -27,7 +84,7 @@ function NavLinks({ handleClick }) {
         );
       })}
       <li className="nav-item">
-        <a href={"/resume.pdf"} target="_blank" rel="noopener noreferrer">
+        <a href={"/resume.pdf"} target="_blank" rel="noreferrer">
           Resume
         </a>
       </li>
--- components/Particles.js
+++ components/Particles.js
@@ -3,24 +3,15 @@ import { loadFull } from "tsparticles";
 
 export const ParticlesBackground = () => {
   const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
   };
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <Particles
       id="tsparticles"
       className="particles-wrapper"
       canvasClassName="particles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         background: {
           color: {
