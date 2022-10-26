990475b0cd0a6c7c9ede1124dcdddec30b395a71
Kailon Simon <81045491+KailonSimon@users.noreply.github.com>
2022-10-26 00:25:03 -0500

    Fix body scroll bug

--- sections/Hero.js
+++ sections/Hero.js
@@ -6,8 +6,7 @@ import AnimatedMouse from "../components/AnimatedMouse";
 const useStyles = createStyles((theme) => ({
   content: {
     width: "100%",
-    minHeight:
-      "calc(100vh - 4rem - env(safe-area-inset-bottom) - env(safe-area-inset-top))",
+    minHeight: "calc(100vh - 4rem)",
     maxWidth: 1280,
     display: "flex",
     flexDirection: "column",
