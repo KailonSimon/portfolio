ed7af4a44cc64d88e797c11348b32decce3ebdb6
KailonSimon <81045491+KailonSimon@users.noreply.github.com>
2024-08-01 10:20:23 -0500

    Force dynamic fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import HeroSection from "./components/HeroSection";
 import ProjectSection from "./components/ProjectSection";
 import { getLanguagesData } from "./utils/getLanguagesData";
 
+export const dynamic = 'force-dynamic'
+
 async function getProjectsData() {
   const res = await fetch(
     `${process.env.CONTENT_URL}/api/projects?populate[0]=thumbnail&populate[1]=tech_badges.image`
